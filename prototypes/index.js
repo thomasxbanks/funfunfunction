e51308b405d764477d76362b3fed75a21b79677b
Thomas Banks <thomasxbanks@gmail.com>
2016-08-17 14:20:05 +0100

    extended prototypes

--- prototypes/index.js
+++ prototypes/index.js
@@ -1,20 +1,35 @@
 const food = {
-  init: function (type) {
-    this.type = type
-  },
-  eat: function() {
-    console.log("you ate the " + this.type)
-  }
+    init: function(type, method) {
+        this.type = type,
+        this.method = method
+    },
+    eat: function() {
+        console.log("you eat the " + this.type)
+    },
+    cook: function() {
+        console.log("you " + this.method + " the " + this.type)
+    }
 }
 
 const waffle = Object.create(food)
-waffle.init('waffle')
+waffle.init('waffle', 'griddle')
+waffle.cook()
 waffle.eat()
 
+food.init('bacon', 'grill')
+food.cook()
+food.eat()
+
 const carrot = Object.create(food)
-carrot.init('carrot')
+carrot.init('carrot', 'boil')
+carrot.cook()
 carrot.eat()
 
-console.log("waffle is food", food.isPrototypeOf(waffle))
-console.log("int is food", food.isPrototypeOf(654654654))
-console.log("carrot is food", food.isPrototypeOf(carrot))
+food.init('eggs', 'fry')
+food.cook()
+food.eat()
+
+
+console.log("waffle is food:", food.isPrototypeOf(waffle))
+console.log("int is food:", food.isPrototypeOf(654654654))
+console.log("carrot is food:", food.isPrototypeOf(carrot))
