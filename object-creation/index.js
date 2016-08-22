82b715edcf95556bdcdff6fae7b547837d8c4204
thomasxbanks <thomasxbanks@gmail.com>
2016-08-22 21:04:12 +0100

    The racoon that disproves the rule.

--- object-creation/index.js
+++ object-creation/index.js
@@ -4,20 +4,25 @@ function describe() {
     let petName = this.name || this.type // If your pet has no name, refer to it by its type
     let petActivity = this.activity || 'walk' // What does your pet like to do?
     let petSound = this.sound || 'nothing' // What does the fox say? PS Note the Beach Boys reference!
-    let petHate = this.dislike || 'anything' // Does your pet hate anything?
+    let petHate = this.dislike || 'nothing' // Does your pet hate anything?
     // Spit this to the console, brah!
     console.log(petName + " likes to " + petActivity + " and says " + petSound + " but doesn't like " + petHate)
 }
 
 const Animal = {
-    dislike: 'baths', // All animals hate baths, irrespective
     describe // run teh describe function
+}
+
+const Racoon = {
+  type: 'racoon',
+  sound: 'silent (like your screams)'
 }
 
 const Dog = {
     type: "dog",
     activity: "run",
-    sound: "woof"
+    sound: "woof",
+    dislike: 'baths'
 }
 
 const Corgi = {
@@ -33,9 +38,11 @@ const Alfie = {
 }
 
 Object.setPrototypeOf(Dog, Animal) // Dog is a type of animal
+Object.setPrototypeOf(Racoon, Animal) // Dog is a type of animal
 Object.setPrototypeOf(Corgi, Dog) // Corgi is a type of dog
 Object.setPrototypeOf(Alfie, Corgi) // Alfie is a type of corgi, which is also a type of dog
 
 Dog.describe()
 Corgi.describe()
 Alfie.describe()
+Racoon.describe()
